ef41ffa3bfcc0b1088298e9c6395a449de5b0988
goddesGeek <goddesses.geek@gmail.com>
2024-08-13 19:10:04 +0700

    Create blog-post-page.js
    making page for blog post

---
+++ blog-post-page.js
@@ -0,0 +1,41 @@
+jQuery(document).ready(function(){
+jQuery(&#39;.button_1&#39;).click(function()
+{
+jQuery(&#39;.content_1&#39;).fadeIn(&#39;slow&#39;);
+jQuery(&#39;.content_2&#39;).fadeOut(&#39;fast&#39;);
+jQuery(&#39;.content_3&#39;).fadeOut(&#39;fast&#39;);
+jQuery(this).css(&#39;background&#39;,&#39;#F4655F&#39;);
+jQuery(this).css(&#39;color&#39;,&#39;#fff&#39;);
+jQuery(&#39;.button_2&#39;).css(&#39;background&#39;,&#39;#fff&#39;);
+jQuery(&#39;.button_2&#39;).css(&#39;color&#39;,&#39;#F4655F&#39;);
+jQuery(&#39;.button_3&#39;).css(&#39;background&#39;,&#39;#fff&#39;);
+jQuery(&#39;.button_3&#39;).css(&#39;color&#39;,&#39;#F4655F&#39;);
+return false;
+});
+jQuery(&#39;.button_2&#39;).click(function()
+{
+jQuery(&#39;.content_1&#39;).fadeOut(&#39;fast&#39;);
+ jQuery(&#39;.content_2&#39;).fadeIn(&#39;slow&#39;);
+jQuery(&#39;.content_3&#39;).fadeOut(&#39;fast&#39;);
+jQuery(this).css(&#39;background&#39;,&#39;#F4655F&#39;);
+jQuery(this).css(&#39;color&#39;,&#39;#fff&#39;);
+jQuery(&#39;.button_1&#39;).css(&#39;background&#39;,&#39;#fff&#39;);
+jQuery(&#39;.button_1&#39;).css(&#39;color&#39;,&#39;#F4655F&#39;);
+jQuery(&#39;.button_3&#39;).css(&#39;background&#39;,&#39;#fff&#39;);
+jQuery(&#39;.button_3&#39;).css(&#39;color&#39;,&#39;#F4655F&#39;);
+return false;
+});
+jQuery(&#39;.button_3&#39;).click(function()
+{
+jQuery(&#39;.content_1&#39;).fadeOut(&#39;fast&#39;);
+jQuery(&#39;.content_2&#39;).fadeOut(&#39;fast&#39;);
+jQuery(&#39;.content_3&#39;).fadeIn(&#39;slow&#39;);
+jQuery(this).css(&#39;background&#39;,&#39;#F4655F&#39;);
+jQuery(this).css(&#39;color&#39;,&#39;#fff&#39;);
+jQuery(&#39;.button_1&#39;).css(&#39;background&#39;,&#39;#fff&#39;);
+jQuery(&#39;.button_1&#39;).css(&#39;color&#39;,&#39;#F4655F&#39;);
+jQuery(&#39;.button_2&#39;).css(&#39;background&#39;,&#39;#fff&#39;);
+jQuery(&#39;.button_2&#39;).css(&#39;color&#39;,&#39;#F4655F&#39;);
+return false;
+});
+});
